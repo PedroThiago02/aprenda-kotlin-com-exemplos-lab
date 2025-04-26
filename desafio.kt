b98b97ab873126c3cf223843af901e97134ff00b
Pedro Thiago <144968307+PedroThiago02@users.noreply.github.com>
2025-04-26 15:54:30 -0300

    Finalização do projeto: Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,68 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import kotlin.text.uppercase //Importando a Função UpperCase()
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String, val id: Int)
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var nivel: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
+    //Adicionando um Usuário e Verificando se Houve Alguma Duplicação de Usuários
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if(usuario !in inscritos) {
+            inscritos.add(usuario)
+            println("O usuário ${usuario.nome.uppercase()}, portador do id: (${usuario.id}), agora está matriculado na formação ${nome.uppercase()}. Nível: $nivel!")
+            println("") //Espaço de Parágrafo Para Melhor Visibilidade na Saída do Terminal
+        } else {
+            println("O usuário ${usuario.nome} já está matriculado!".uppercase())
+            println("") //Espaço de Parágrafo Para Melhor Visibilidade na Saída do Terminal
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    //Início de Usúarios Inscritos na Plataforma
+    
+    val usuario1 = Usuario("Pedro Thiago", 23)
+    val usuario2 = Usuario("Ezequiel", 45)
+    val usuario3 = Usuario("Carlos", 56)
+    
+    //Final de Usúarios Inscritos na Plataforma
+    
+    //Início Conteúdos de Cada Usúarios
+    
+    val conteudos_kotlin = listOf(
+        ConteudoEducacional("Tratamento de Exceções em Kotlin", 120),  
+        ConteudoEducacional("Programação Orientada a Objetos", 180)
+    )
+    
+    val conteudos_PHP = listOf(
+        ConteudoEducacional("Introdução a PHP", 120),  
+        ConteudoEducacional("Programação Orientada a Objetos", 180)
+    )
+    
+    //Final Conteúdos de Cada Usúarios
+    
+    //Início de Formações De cada Usúario
+    
+    val formacao_usuario_kotlin = Formacao("Kotlin Back-end Developer", "Intermediário".uppercase(), conteudos_kotlin)
+    val formacao_usuario_PHP = Formacao("PHP Experience", "Avançado".uppercase(), conteudos_PHP)
+    
+    
+    //Final de Formações De cada Usúario
+    
+    //Início das Matriculações de Cada Usúario em Formações
+    
+    formacao_usuario_kotlin.matricular(usuario1)
+    formacao_usuario_PHP.matricular(usuario2)
+    formacao_usuario_kotlin.matricular(usuario1) //Teste de duplicação
+    formacao_usuario_kotlin.matricular(usuario3)
+    
+    //Final das Matriculações de Cada Usúario em Formações
+    
+    println("Inscritos na Formação Kotlin Back-end Developer: ${formacao_usuario_kotlin.inscritos.map {it.nome}}") //Imprimindo o Nome dos Inscritos na Formação Kotlin
+    println("Inscritos na Formação PHP Experience: ${formacao_usuario_PHP.inscritos.map {it.nome}}") //Imprimindo o Nome dos Inscritos na Formação PHP Experience
+    
 }
